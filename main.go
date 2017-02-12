84f176011ea0f49ef7885a8ed113409dae973f04
Rich Dillon <richraid21@gmail.com>
2017-02-12 13:54:31 -0500

    Intitial Commit w/ README

---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"io/ioutil"
+	"log"
+	"strings"
+	"os/exec"
+	"os"
+)
+
+type ScriptExecution struct {
+	instance string
+	database string
+	path string
+	filename string
+}
+
+func (se *ScriptExecution) Cmd() string{
+	return `sqlcmd -S `+se.instance+` -d `+se.database+` -i `+se.path
+}
+
+func main() {
+
+	args := os.Args[1:]
+	connectionString := os.Getenv("GOSQLCONNECTION")
+	pair := strings.Split(connectionString,":")
+	
+	instanceString := pair[0]
+	databaseName := pair[1]
+	folderPath := args[0]
+	
+	
+	files, err := ioutil.ReadDir(folderPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	for _, file := range files {
+	
+		fileName := file.Name()
+		
+		if strings.HasSuffix(fileName,".sql") {
+			se := ScriptExecution{instance: instanceString, database: databaseName, path: folderPath+`\`+fileName, filename: fileName}
+			run(&se)
+		}
+		
+	}
+
+	numFiles := strconv.Itoa(len(files))
+	fmt.Println(`Executed `+numFiles+` of `+numFiles+` files.`)
+}
+
+func run(eo *ScriptExecution){
+	
+	execCmd := eo.Cmd()	
+	
+	lsCmd := exec.Command("cmd", "/C", execCmd)
+    lsOut, err := lsCmd.Output()
+    if err != nil || strings.HasPrefix(string(lsOut),"Msg") {
+        fmt.Println(`SQL Script: `+eo.filename+` encountered an error during execution.`)
+		fmt.Println(string(lsOut))
+		fmt.Println(err)
+		log.Fatal(err)
+    }
+}
+
